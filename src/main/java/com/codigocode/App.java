0e18e67f8678c7cbc4916cbfa3b483b6c1dd8670
Neoris <Neoris>
2020-11-03 11:24:31 -0300

    first commit

---
+++ src/main/java/com/codigocode/App.java
@@ -0,0 +1,13 @@
+package com.codigocode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! hola codigocode hola codinglab" );
+    }
+}
